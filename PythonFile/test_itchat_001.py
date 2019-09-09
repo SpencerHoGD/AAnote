ac87ae2eeba4e9fb0134ac48a32956e46a195323
tencent_ubuntu <youyouchangkong@163.com>
2019-09-09 18:06:53 +0800

    add six py

---
+++ PythonFile/test_itchat_001.py
@@ -0,0 +1,8 @@
+#coding:utf-8
+#author:hexiaoming
+
+import itchat
+
+itchat.auto_login()
+
+itchat.send('Hello, filehelper', toUserName='filehelper')
---
+++ PythonFile/test_stepbystep.py
@@ -0,0 +1,12 @@
+#coding:utf-8
+#author:hexiaoming
+
+import os
+path = "D:/rename"
+filelist = os.listdir(path)
+
+count = 0
+
+for file in filelist:   
+    Olddir=os.path.join(path,file)
+    print(Olddir)
