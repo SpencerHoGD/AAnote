f780a4c91917bb97be51bc7adaa12c82792b7cd6
tencent_ubuntu <youyouchangkong@163.com>
2019-10-10 14:56:23 +0800

    add some file

--- PythonFile/test001.py
+++ PythonFile/test001.py
@@ -13,4 +13,7 @@ import re
 d = r'D:\work'  # 文件所在的文件夹路径
 fileList = os.listdir(d)
 
-print(fileList)
+nowTime = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+startDir  = r'\\192.168.106.201\风控中心\监察部\4. 监察方案'
+filelistlog = path.join("D:\\fileListLog" + str(nowTime) + ".md")  # 保存文件路径)
+print(filelistlog)
