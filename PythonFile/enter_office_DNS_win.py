9b6370bef78d6921845020a3f389aa46c2873b4e
tencent_ubuntu <youyouchangkong@163.com>
2019-10-10 09:58:47 +0800

    move 2 py file

---
+++ PythonFile/enter_office_DNS_win.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+'''
+Created on 2019-10-10
+hexiaoming
+'''
+
+
+import os
+
+d = r'\\192.168.106.201\风控中心综合体系\09、资金计划、报销、福利、资产管理'
+#os.system("explorer.exe %s" % path)
+#os.startfile(path)
+os.system('notepad')
+#fileList = os.listdir(d)
+
+#print(fileList)
---
+++ PythonFile/test001.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+'''
+Created on 2019-10-09
+hexiaoming
+'''
+
+import os
+from os import path
+import datetime
+import re
+
+
+d = r'D:\work'  # 文件所在的文件夹路径
+fileList = os.listdir(d)
+
+print(fileList)
