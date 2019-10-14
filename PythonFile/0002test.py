d10ae1a446e0377b189770a051bd7f3550675e1d
tencent_ubuntu <youyouchangkong@163.com>
2019-10-14 17:57:22 +0800

    add some pythonfiles

--- PythonFile/0002test.py
+++ PythonFile/0002test.py
@@ -1,11 +1,16 @@
 import os
-from os import path
-from shutil import copyfile
 
-#源文件夹
-srcDir = r'\\192.168.106.201\风控中心综合体系\07、文件模板、函件、通讯\7.2 函件管理\2019年'
-#目标文件夹
-dstDir = r"D:\\copy_pdf"
-#如果目录不存在，则创建
-if not path.isdir(dstDir):
-    os.makedirs(dstDir)
+path = r"d:\read_write_test"
+
+i = 0
+pathList = []
+for (path, dirs, files) in os.walk(path):
+    print("////////////////////////////////////////////////////////////////////////")
+    print(path)
+    # print(dirs)
+    print(files)
+    # pathList.append(path)
+    print("-------------")
+
+
+# print(pathList)
