ca97adb9efa3e01a0e25c85657dddcb17ab4ded0
tencent_ubuntu <youyouchangkong@163.com>
2019-10-30 18:00:20 +0800

    add some pyfile

--- PythonFile/0004test.py
+++ PythonFile/0004test.py
@@ -1,21 +1,15 @@
-from random import randint, sample
-from time import time, sleep
+import hashlib
+import random
 
-while True:
-    # 初始化备选红色球
-    red_balls = [x for x in range(1, 34)]
-    # 选出六个红色球
-    selected_balls = sample(red_balls, 6)
-    # 对红色球进行排序
-    selected_balls.sort()
-    # 添加一个蓝色球
-    selected_balls.append(randint(1, 16))
-    # 输出选中的随机号码
-    for index, ball in enumerate(selected_balls):
-        print('%02d' % ball, end=' ')
-        if index == len(selected_balls) - 2:
-            print('|', end=' ')
-    print()
-    sleep(1)
-    
-    
+
+# m = random.getrandbits(256)
+# print(m)
+# n = hashlib.sha256(str(random.getrandbits(256)).encode('utf-8')).hexdigest()
+# print(n)
+
+m = hashlib.sha256()
+m.update(b"Nobody inspects")
+# m.update(b" the spammish repetition")
+# x = m.hexdigest()
+x = str(m.digest())
+print(x)
