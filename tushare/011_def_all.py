926c03f41ca07b1827699063fefed9b70d3eefc4
tencent_ubuntu <youyouchangkong@163.com>
2019-12-12 17:43:13 +0800

    update

--- tushare/011_def_all.py
+++ tushare/011_def_all.py
@@ -12,9 +12,11 @@ def ts_pro_api():
     return pro
 
 
+
+
+
 def connectPostgreSQL():
-    conn = psycopg2.connect(database='postgres', user='postgres', 
-                            password='394460', host='127.0.0.1', port=5432)
+    conn = psycopg2.connect(database='postgres', user='postgres', password='394460', host='127.0.0.1', port=5432)
     print('connect successful!')
     
     cursor=conn.cursor()
@@ -29,10 +31,72 @@ def connectPostgreSQL():
     print('table public.member is created!')
 
 
-def close_db_connection(conn):
+def insertOperate():
+    conn = psycopg2.connect(database='postgres', user='postgres', password='394460', host='127.0.0.1', port=5432)
+    cursor=conn.cursor()
+    cursor.execute("insert into public.member(id,name,password,singal)\
+        values(1,'member0','password0','signal0')")
+    cursor.execute("insert into public.member(id,name,password,singal)\
+        values(2,'member1','password1','signal1')")
+    cursor.execute("insert into public.member(id,name,password,singal)\
+        values(3,'member2','password2','signal2')")
+    cursor.execute("insert into public.member(id,name,password,singal)\
+        values(4,'member3','password3','signal3')")
+    
+    # for i in range(5, 101):
+    #     cursor.execute("insert into public.member(id,name,password,singal)\
+    #         values({i},'member{i}','password{i}','signal{i})")
     conn.commit()
     conn.close()
+
+    print('insert records into public.memmber successfully')
+
+
+def selectOperate():
+    conn = psycopg2.connect(database='postgres', user='postgres', password='394460', host='127.0.0.1', port=5432)
+    cursor=conn.cursor()
+    cursor.execute("select id,name,password,singal from public.member where id>0")
+    rows=cursor.fetchall()
+    for row in rows:
+        print('id=',row[0], ',name=',row[1],',pwd=',row[2],',singal=',row[3], '\n')
+    conn.close()
+
+
+def updateOperate():
+    conn = psycopg2.connect(database='postgres', user='postgres', password='394460', host='127.0.0.1', port=5432)
+    cursor = conn.cursor()
+    cursor.execute("update public.member set name='update ...' where id=2")
+    conn.commit()
+    print("Total number of rows updated :", cursor.rowcount)
+
+    cursor.execute("select id,name,password,singal from public.member")
+    rows=cursor.fetchall()
+    for row in rows:
+        print('id=',row[0], ',name=',row[1],',pwd=',row[2],',singal=',row[3],'\n')
+    conn.close()
+
     
+def deleteOperate():
+    conn = psycopg2.connect(database='postgres', user='postgres', password='394460', host='127.0.0.1', port=5432)
+    cursor=conn.cursor()
+
+    cursor.execute("select id,name,password,singal from public.member")
+    rows=cursor.fetchall()
+    for row in rows:
+        print('id=',row[0], ',name=',row[1],',pwd=',row[2],',singal=',row[3],'\n')
+
+    print('begin delete')
+    cursor.execute("delete from public.member where id=4")
+    conn.commit()   
+    print('end delete')
+    print("Total number of rows deleted :", cursor.rowcount)
+    
+    cursor.execute("select id,name,password,singal from public.member")
+    rows=cursor.fetchall()
+    for row in rows:
+        print('id=',row[0], ',name=',row[1],',pwd=',row[2],',singal=',row[3],'\n')
+    conn.close()
+
 
 def create_one_engine():
     engine = create_engine('postgresql://postgres:394460@localhost:5432/postgres')
@@ -42,5 +106,8 @@ def create_one_engine():
     
 if __name__ == "__main__":
     # ts_pro_api()
-    connectPostgreSQL()
-
+    # connectPostgreSQL()
+    # insertOperate()
+    # selectOperate()
+    # updateOperate()
+    deleteOperate()
