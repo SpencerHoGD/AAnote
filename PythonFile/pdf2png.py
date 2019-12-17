f3952e0d80141100e32347c638e93aaddc88112f
tencent_ubuntu <youyouchangkong@163.com>
2019-12-17 17:55:57 +0800

    update

--- PythonFile/pdf2png.py
+++ PythonFile/pdf2png.py
@@ -9,7 +9,8 @@ from os import path
 
 workdir = r'd:\pdf2png'
 
-pdf = 'weixin.pdf'
+pdf = '1.pdf'
+pdf1 = '2.pdf'
 
 fname = path.join(workdir, pdf)
 doc = fitz.open(fname)                      # open document
@@ -17,8 +18,8 @@ doc = fitz.open(fname)                      # open document
 for page in doc:                            # iterate through the pages
     rotate = int(0)
     # 每个尺寸的缩放系数为2，这将为我们生成分辨率提高四倍的图像。
-    zoom_x = 2.0
-    zoom_y = 2.0
+    zoom_x = 1.0
+    zoom_y = 1.0
     trans = fitz.Matrix(zoom_x, zoom_y).preRotate(rotate)
     pix = page.getPixmap(matrix = trans, alpha = False)     # render page to an image
     pix.writePNG(path.join(workdir, "p-%i.png" % (page.number + 1)))    # store image as a PNG
--- tushare/021_new_share.py
+++ tushare/021_new_share.py
@@ -10,8 +10,8 @@ def connectPostgreSQL():
     ts.set_token('74eabb1fce9fd5c317fd38477a465d0aa4eb167e0ee272be91631a0e')
     pro = ts.pro_api()
 
-    year1 = 2000
-    year2 = 2009
+    year1 = 2010
+    year2 = 2019
     start_date = '{}0101'.format(year1)
     end_date = '{}1231'.format(year2)
     df = pro.new_share(start_date=start_date, end_date=end_date)
