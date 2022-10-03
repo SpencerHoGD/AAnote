7d0959dd8ccff801979900dd7601519c12b712bf
hxm-5600X <125423559@qq.com>
2022-10-04 03:35:37 +0800

    ipipdb

---
+++ PythonFile/ipip_database.py
@@ -0,0 +1,16 @@
+import ipdb
+
+import ipdb
+
+db = ipdb.City(r'C:\Users\hxm\Downloads\ipipfreedb\ipipfree.ipdb')
+
+print(db.is_ipv4(), db.is_ipv6())
+print(db.languages())
+print(db.fields())
+print(db.build_time())
+print(db.find("1.12.13.255", "CN"))
+print(db.find_map("1.12.13.255", "CN"))
+print(db.find_info("1.12.13.255", "CN").country_name)
+
+
+# print(db.())
